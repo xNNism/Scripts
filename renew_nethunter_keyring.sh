5ab08d99509d8d01d2b8cb243ed19015fd9ee9de
xNN <xnnism@gmail.com>
2018-10-29 10:38:37 +0100

    Update renew_nethunter_keyring.sh

--- renew_nethunter_keyring.sh
+++ renew_nethunter_keyring.sh
@@ -27,3 +27,21 @@ else
 	echo -n "${ylw} ..finished ${txtrst}"
 	exit
 fi
+
+echo ""
+echo "${ylw} metapackages to install:.${txtrst}"
+echo ""
+sleep 2
+
+(kali-linux
+kali-linux-all
+kali-linux-forensic
+kali-linux-full
+kali-linux-gpu
+kali-linux-pwtools
+kali-linux-rfid
+kali-linux-sdr
+kali-linux-top10
+kali-linux-voip
+kali-linux-web
+kali-linux-wireless
