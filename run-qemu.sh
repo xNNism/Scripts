bb69ad1cc31b33f4b1b08a4d0cce3a92456c868e
xNN <xnnism@gmail.com>
2018-11-13 16:06:27 +0100

    run-qemu.sh: add bootmodes

--- run-qemu.sh
+++ run-qemu.sh
@@ -27,19 +27,36 @@ txtrst=$(tput sgr0) # Reset
 ###################################
 ##         START SCRIPT          ##
 ###################################
+
+## DEVICE SELECTION
+# LIST_DEVS=$(echo -e "$(ls -l /dev/sd*)")
 #
-#  LIST_DEVS=$(echo -e "$(ls -l /dev/sd*)")
+echo
+echo
 LIST_DEVS=$(echo -e "$(lsblk)")
 
 echo "$LIST_DEVS"
 echo
-echo -n "${bldylw}Enter a device to run in QEMU and press [ENTER]...: /dev/ ${txtrst}"
+echo -n "${bldylw}# Enter a device to run in QEMU and press [ENTER]...: /dev/ ${txtrst}"
 read input
 echo
 
+## SELECT BOOTMODE
+#
+echo
+echo "${bldylw}# Boot /dev/$input via UEFI? ${txtrst}${wht}(for UEFI boot OVMF/Tianocore must be installed!) ${txtrst}"
+select yn in "UEFI" "BIOS"; do
+    case $yn in
+        UEFI ) BOOT="--bios /usr/share/OVMF/OVMF_CODE.fd"; break;;
+        BIOS ) BOOT=""; break;;
+    esac
+done
+
+echo
 #
 sudo qemu-system-x86_64 -machine type=pc,accel=kvm -enable-kvm -cpu host -smp cores=2 \
 -m size=2G \
 -vga std \
 -k de \
+$BOOT \
 -hda /dev/$input \
